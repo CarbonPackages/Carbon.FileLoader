aef3ed06c8c119d405b2ca4474733c2367a8fbd0
Jon Uhlmann <account@uhlmann.pro>
2024-07-04 14:25:22 +0200

    New: Add helper method for register events

---
+++ Resources/Private/Assets/EventListener.ts
@@ -0,0 +1,8 @@
+function eventListener(name, callback, once = true) {
+    if (typeof name !== 'string' || typeof callback !== 'function') {
+        throw new Error('eventListener: name must be a string and callback must be a function');
+    }
+    document.addEventListener(name, (event) => callback(event), { once });
+}
+
+export { eventListener as default, eventListener };
